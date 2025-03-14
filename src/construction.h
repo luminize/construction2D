7ae49f43a74ebe8d4511a7dcc254babea642f5fc
bdebruijn <bdebruijn@luminize.nl>
2025-03-14 21:07:04 +0100

    add vector of elements to construction

--- src/construction.h
+++ src/construction.h
@@ -4,10 +4,13 @@
 #include <vector>
 #include <string>
 
+#include "elements.h"
+
 class Construction
 {
 private:
     std::string name;
+    std::vector<Element> construction_elements;
 public:
     Construction();
     Construction(std::string construction_name);

7c561f640807ec646c47d2d681dd9eaebdfd4992
bdebruijn <bdebruijn@luminize.nl>
2025-03-15 17:38:58 +0100

    add json library

--- src/vakwerk.cpp
+++ src/vakwerk.cpp
@@ -1,10 +1,20 @@
 #include "box2d/box2d.h"
 #include "raylib.h"
 #include "construction.h"
+#include "json.hpp"
 
 #include <iostream>
+#include <fstream>
+
+using json = nlohmann::json;
 
 int main(){
     Construction vakwerk("constructie");
+    std::ifstream jsonfile("jsonfile.json");
+  
+    std::ifstream f("example.json");
+    json data = json::parse(f);
+
     std::cout << vakwerk.getname();
+    std::cout << data;
 }
